498d3c0e8b4c761447ea193f9bb93dd402ec9a4e
Eason13245 <65231444+Eason13245@users.noreply.github.com>
2020-05-19 20:03:58 +0900

    算出圓的面積

---
+++ untitled3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 20:01:00 2020
+
+@author: gaoyixun
+"""
+
+
+Radius=input("What is the radius for the circle?")
+print(3.14)
+print("The area is "+str(3.14*(int(Radius)**2)))
