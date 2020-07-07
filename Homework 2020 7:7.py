c6b5941fe6f3da8d1e6fbac864e36514a8f96126
Eason13245 <65231444+Eason13245@users.noreply.github.com>
2020-07-07 19:28:54 +0900

    回家作業（未完成）

---
+++ Homework 2020 7:7.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  7 15:01:33 2020
+
+@author: gaoyixun
+"""
+
+English=[]
+Chinese=[]
+
+ for x in range(0,1*n):
+            A = input(English[x])
+            if A==Chinese[x]:
+                print('Correct')
