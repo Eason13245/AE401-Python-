43eb75a7a7fbb7b8c676c22ff24b36f30d8a7bfc
Eason13245 <65231444+Eason13245@users.noreply.github.com>
2020-08-04 21:07:38 +0900

    第一題

---
+++ 第一題.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  4 20:07:35 2020
+
+@author: gaoyixun
+"""
+
+def EvenTest(n):
+    if n // 2 == 0:
+        print("偶數")
+    else:
+        print("奇數")
+
